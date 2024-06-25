5a4d3bb5a7f26a686a43e641fa1d0d1f90b0b36f
Grigorash Pavel <grigorash.pavel.it@yandex.ru>
2024-06-25 22:01:13 +0300

    feature: создано поле сообщения дела

---
+++ app/src/main/java/com/pasha/ytodo/presentation/edit/compose/TodoTextField.kt
@@ -0,0 +1,48 @@
+package com.pasha.ytodo.presentation.edit.compose
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.pasha.ytodo.R
+
+
+@Composable
+fun TodoTextField(
+    text: String,
+    onTextChanged: (String) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    ElevatedCard(
+        shape = RoundedCornerShape(8.dp),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = dimensionResource(
+                id = R.dimen.edit_task_todo_text_field_decor_elevation
+            )
+        ),
+        modifier = modifier
+    ) {
+        TextField(
+            value = text,
+            onValueChange = onTextChanged,
+            minLines = 3,
+            placeholder = {
+                Text(text = stringResource(id = R.string.item_task_hint_tip))
+            },
+            colors = TextFieldDefaults.colors(
+                unfocusedIndicatorColor = Color.Transparent,
+                focusedIndicatorColor = Color.Transparent
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
+}
