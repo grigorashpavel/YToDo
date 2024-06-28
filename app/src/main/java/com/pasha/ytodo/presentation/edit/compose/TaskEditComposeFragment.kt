3327d31e7ae91de44b6dccabbd3bc6009c1766fa
Grigorash Pavel <grigorash.pavel.it@yandex.ru>
2024-06-28 16:55:50 +0300

    feature: добавлен фрагмент Compose экрана

--- app/src/main/java/com/pasha/ytodo/presentation/edit/compose/TaskEditComposeFragment.kt
+++ app/src/main/java/com/pasha/ytodo/presentation/edit/compose/TaskEditComposeFragment.kt
@@ -5,14 +5,36 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
-import com.pasha.ytodo.R
+import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
+import androidx.navigation.findNavController
+import com.pasha.ytodo.presentation.TodoItemViewModel
+import com.pasha.ytodo.presentation.edit.views.EditTaskViewModel
 
 
 class TaskEditComposeFragment : Fragment() {
+    private val todoItemViewModel by activityViewModels<TodoItemViewModel>()
+    private val viewModel by viewModels<EditTaskViewModel> {
+        EditTaskViewModel.provideFactory()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val task = todoItemViewModel.sharedTodoItem
+        val isFirstCreation = savedInstanceState == null
+        if (isFirstCreation && task != null) {
+            viewModel.changePriority(task.priority)
+            viewModel.changeDeadline(task.deadline)
+        }
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -20,9 +42,18 @@ class TaskEditComposeFragment : Fragment() {
         return ComposeView(requireContext()).apply {
             setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
             setContent {
-                MaterialTheme {
-                    Text(text = "Test Compose Screen")
-                }
+                var navigationStarted by rememberSaveable { mutableStateOf(false) }
+                EditTaskScreen(
+                    viewModel,
+                    todoItemViewModel,
+                    navigateBackAction = {
+                        if (!navigationStarted) {
+                            navigationStarted = true
+
+                            findNavController().navigateUp()
+                        }
+                    }
+                )
             }
         }
     }
