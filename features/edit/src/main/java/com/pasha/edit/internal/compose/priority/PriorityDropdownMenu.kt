ac0716315342e5851e87925fe8630e537bebcd4f
Grigorash Pavel <grigorash.pavel.it@yandex.ru>
2024-07-16 20:04:12 +0300

    feature: добавлена цветовая индикация по нажатию на опции дедлайна и приоритета

--- features/edit/src/main/java/com/pasha/edit/internal/compose/priority/PriorityDropdownMenu.kt
+++ features/edit/src/main/java/com/pasha/edit/internal/compose/priority/PriorityDropdownMenu.kt
@@ -1,7 +1,16 @@
 package com.pasha.edit.internal.compose.priority
 
+import androidx.compose.foundation.LocalIndication
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.indication
+import androidx.compose.foundation.interaction.Interaction
+import androidx.compose.foundation.interaction.InteractionSource
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.PressInteraction
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -11,12 +20,15 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
@@ -24,6 +36,7 @@ import androidx.compose.ui.unit.dp
 import com.pasha.core_ui.R
 import com.pasha.domain.entities.TaskPriority
 import com.pasha.edit.internal.compose.share.OptionPickerTextField
+import kotlinx.coroutines.flow.collectLatest
 
 
 @Composable
@@ -35,9 +48,19 @@ fun PriorityDropdownMenu(
     options: List<TaskPriority> = getPriorityOptions()
 ) {
     var expanded by remember { mutableStateOf(false) }
+    val interactionSource = remember {
+        MutableInteractionSource()
+    }
+    val focusManager = LocalFocusManager.current
+    LaunchedEffect(key1 = expanded) {
+        focusManager.clearFocus()
+    }
+
     ExposedDropdownMenuBox(
         expanded = expanded,
-        onExpandedChange = { expanded = it },
+        onExpandedChange = {
+            expanded = it
+        },
         modifier = modifier
     ) {
         OptionPickerTextField(
@@ -46,7 +69,11 @@ fun PriorityDropdownMenu(
             textStyle = TextStyle(color = getOptionColor(baseOption)),
             modifier = Modifier
                 .fillMaxWidth()
-                .menuAnchor(),
+                .menuAnchor()
+                .indication(
+                    interactionSource,
+                    rememberRipple(color = colorResource(id = R.color.color_blue))
+                ),
             label = {
                 Text(
                     text = stringResource(
@@ -62,12 +89,15 @@ fun PriorityDropdownMenu(
                 focusedContainerColor = Color.Transparent,
                 unfocusedContainerColor = Color.Transparent
             ),
+            interactionSource = interactionSource,
             contentPadding = PaddingValues(vertical = 8.dp)
         )
         DropdownMenu(
             expanded = expanded,
             modifier = Modifier.exposedDropdownSize(),
-            onDismissRequest = { expanded = false }
+            onDismissRequest = {
+                expanded = false
+            }
         ) {
             options.forEach { option ->
                 DropdownMenuItem(
